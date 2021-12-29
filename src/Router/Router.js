b06563adf899b7452b64dda2fd24912f3c39779b
Hyperactive-knucklehead <htcrayhan2015@outlook.com>
2021-12-29 18:28:49 +0600

    Registration page added

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ForgotPassword from "../Components/ForgotPassword/ForgotPassword";
 import Home from "../Components/Home/Home";
 import Login from "../Components/Login/Login";
+import Registration from "../Components/Registration/Registration";
 import NavigationBar from "../Components/Shared/NavigationBar/NavigationBar";
 import Signup from "../Components/Signup/Signup";
+import PrivateRoute from "../Private/PrivateRoute";
 
 const AppRouter = () => {
   return (
@@ -16,6 +18,14 @@ const AppRouter = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route
+          path="/register/:eventId"
+          element={
+            <PrivateRoute>
+              <Registration />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
