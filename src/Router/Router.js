7467baede652d9893d7fb538f9d6797cbde3a4a9
Hyperactive-knucklehead <htcrayhan2015@outlook.com>
2021-12-29 18:15:28 +0600

    firebase authentication added (login signup)

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "../Components/ForgotPassword/ForgotPassword";
 import Home from "../Components/Home/Home";
+import Login from "../Components/Login/Login";
 import NavigationBar from "../Components/Shared/NavigationBar/NavigationBar";
+import Signup from "../Components/Signup/Signup";
 
 const AppRouter = () => {
   return (
@@ -9,6 +12,10 @@ const AppRouter = () => {
       <NavigationBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
